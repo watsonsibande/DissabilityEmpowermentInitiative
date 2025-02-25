6286551108004ad7b8e8f02e589ea5931effb217
watsonsibande <watsonsibande@gmail.com>
2025-02-25 18:58:34 +0200

    Add files via upload
    dei

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php include('includes/db.php'); ?>
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+
+<div class="home-content">
+<div class="hero-section">
+        <div class="hero-content">
+            <h1>Dissability Empowerment Initiative </h1>
+            <p>inclusion for all, Equality for Everyone</p>
+        </div>
+    </div>
+
+    <div class="hero-section-2">
+    <div class="introduction">
+        <h2> Vision</h2>
+        <p>A world where people with disabilities are empowered to reach their full
+            potential, living independently and contributing to their communities with dignity
+            and respect.</p>
+    </div>
+
+    <div class="introduction2">
+    <h2>Mission</h2>
+        <p>DEI empowers people with disabilities through education, advocacy and
+        inclusive opportunities, promoting equal access and challenging societal barriers
+        to create a more inclusive and accepting community.</p>
+    </div>
+    </div>
+
+ 
+
+    </div>
+
+<div class="detail">
+
+    <div class="left">
+        <h1> Organization Believes</h1>
+
+        <ul>
+  <li><strong>Every Individual Has Potential</strong>: We believe that every individual, regardless of their abilities, has unique strengths and talents that can contribute to society. Empowering individuals to realize and harness their potential is at the heart of our mission.</li>
+  <li><strong>Accessibility is a Right</strong>: We believe that accessibility is a fundamental human right. All environments—physical, social, and digital—should be designed to accommodate the diverse needs of individuals with disabilities.</li>
+  <li><strong>Empowerment Through Education and Business</strong>: We believe that access to quality education and meaningful business opportunities are critical for the empowerment and independence of individuals with disabilities. Education and businesses are powerful tools for personal and societal transformation.</li>
+  <li><strong>Community and Belonging</strong>: We believe in the power of community and the importance of belonging. Building inclusive communities where individuals with disabilities feel valued and connected is essential for social well-being and cohesion.</li>
+  <li><strong>Advocacy and Voice</strong>: We believe in the importance of advocacy and giving a voice to individuals with disabilities. We are committed to amplifying their voices in policy-making, public discourse, and community planning.</li>
+  <li><strong>Continuous Learning and Improvement</strong>: We believe in the necessity of continuous learning and improvement. Staying informed about best practices, emerging research, and innovative solutions allows us to better serve individuals with disabilities and adapt to their evolving needs.</li>
+</ul>
+
+      
+</div>
+
+
+<div class="right">
+
+</div>
+
+
+
+</div>
+
+<?php include('includes/footer.php'); ?>
